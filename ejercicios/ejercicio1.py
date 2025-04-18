488f87fdb8bd0bd9be39bdd572bad78af093981b
Karina <karisdext@gmail.com>
2025-04-17 23:21:47 -0300

    Se crea menu principal guia para los demas ejercicios

---
+++ ejercicios/ejercicio1.py
@@ -0,0 +1,4 @@
+
+def run():
+    print('Bienvenido al ejercicio 1')
+    # CODIGO
---
+++ main.py
@@ -0,0 +1,56 @@
+# IMPORTACION DE CARPETA DE EJERCICIOS
+import sys
+sys.path.append('./ejercicios')
+
+# IMPORTACIONES DE MODULOS
+import ejercicio1
+
+"""
+    Presentacion del Trabajo Practico
+    Contiene el menu principal a los ejercicios realizados por los integrates del grupo.
+"""
+def menu():
+    print('+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++')
+    print('- - - - - - - -  TP Grupal de Integracion  - - - - - - - -')
+    print('- - - - - - - - - -   Grupo SlowPoke   - - - - - - - - - -')
+    print('+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n')
+
+    # MENU
+    # Se presentan los ejercicios disponibles, el usuario ingresa una opcion y se ejecuta el ejercicio correspondiente
+    # El menu se repite hasta que el usuario ingrese la opcion 0
+    opcion = ''
+
+    while opcion != '0':
+        print('Ejercicios disponibles (ingrese el numero del ejercicio):')
+        print('')
+        print('1. Ejercicio 1')
+        print('2. Ejercicio 2')
+        print('3. Ejercicio 3')
+        print('4. Ejercicio 4')
+        print('0. Salir')
+
+        opcion = input('Ingrese una opcion: ')
+
+        match opcion:
+            case '1':
+                ejercicio1.run()
+            case '2':
+                print('Ejercicio 2')
+                #ejercicio2()
+            case '3':
+                print('Ejercicio 3')
+                #ejercicio3()
+            case '4':
+                print('Ejercicio 4')
+                #ejercicio4()
+            case '0':
+                print('Gracias por usar nuestro programa! Vuelve pronto!')
+                break
+            case _:
+                print('No es una opcion valida. Intente nuevamente.')
+                
+        print('\n+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n')
+
+
+# EJECUCION DEL MENU PRINCIPAL
+menu()
